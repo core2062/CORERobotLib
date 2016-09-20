234dcaa5449ec3303e6690b9236045e1134a4144
Andrew Kempen <akemperfi@gmail.com>
2016-09-19 21:58:25 -0500

    Added basic COREJoystick code

--- src/COREHardware/COREJoystick.cpp
+++ src/COREHardware/COREJoystick.cpp
@@ -1,10 +1,72 @@
-/*
- * COREJoystick.cpp
- *
- *  Created on: Sep 19, 2016
- *      Author: akemperfi
- */
-
 #include "COREJoystick.h"
 
+using namespace CORE;
 
+COREJoystick::COREJoystick(unsigned int joystickNumber) :
+	joystick(joystickNumber)
+{
+
+}
+
+void COREJoystick::registerAxis(JoystickAxis axis) {
+	axisCache[axis] = joystick.GetRawAxis(axis);
+}
+
+void COREJoystick::registerButton(JoystickButton button) {
+	buttonCache[button] = joystick.GetRawButton(button)  ? ACTIVE : NORMAL;
+}
+
+double COREJoystick::getAxis(JoystickAxis axis) {
+	if(axisCache.find(axis) == axisCache.end()) {
+		//TODO: Error: axis not registered, regestering and returing 0
+		registerAxis(axis);
+		return joystick.GetRawAxis(axis);
+	}
+	else {
+		return axisCache[axis];
+	}
+}
+
+bool COREJoystick::getButton(JoystickButton button) {
+	if(buttonCache.find(button) == buttonCache.end()) {
+		//TODO: Error: button not registered, regestering and returing state
+		registerButton(button);
+		return joystick.GetRawButton(button);
+	}
+	else {
+		return (buttonCache[button] == PRESSED || buttonCache[button] == ACTIVE);
+	}
+}
+
+ButtonState COREJoystick::getButtonState(JoystickButton button) {
+	if(buttonCache.find(button) == buttonCache.end()) {
+		//TODO: Error: button not registered, regestering and returing state
+		registerButton(button);
+		return joystick.GetRawButton(button) ? ACTIVE : NORMAL;
+	}
+	else {
+		return buttonCache[button];
+	}
+}
+
+void COREJoystick::preTeleopTask() {
+	lastButtonCache = buttonCache;
+	for(auto button : buttonCache) {
+		bool isActive = joystick.GetRawButton(button.first);
+		if(lastButtonCache[button.first] == RELEASED || lastButtonCache[button.first] == NORMAL) {
+			if(isActive)
+				button.second = PRESSED;
+			else
+				button.second = NORMAL;
+		}
+		else if (lastButtonCache[button.first] == PRESSED || lastButtonCache[button.first] == ACTIVE) {
+			if(isActive)
+				button.second = ACTIVE;
+			else
+				button.second = RELEASED;
+		}
+	}
+	for(auto axis : axisCache) {
+		axis.second = joystick.GetRawAxis(axis.first);
+	}
+}
--- src/COREHardware/COREJoystick.h
+++ src/COREHardware/COREJoystick.h
@@ -1,61 +1,68 @@
 #ifndef SRC_CORELIBRARY_COREHARDWARE_COREJOYSTICK_H_
 #define SRC_CORELIBRARY_COREHARDWARE_COREJOYSTICK_H_
 #include <vector>
+#include <map>
 #include "../CORESubsystemsManager.h"
 
 
-#ifndef SIMULATION
+#ifdef __arm__
 #include "WPILib.h"
 #endif
 
+using namespace std;
+using namespace CORE;
+
 namespace CORE {
-	enum JoystickAxis {
-		LEFTSTICKX,
-		LEFTSTICKY,
-		RIGHTSTICKX,
-		RIGHTSTICKY
-	};
-	enum JoystickButton {
-		DPADN,
-		DPADNE,
-		DPADE,
-		DPADSE,
-		DPADS,
-		DPADSW,
-		DPADW,
-		DPADNW,
-		LEFTSTICK,
-		RIGHTSTICK,
-		BACKBUTTON,
-		LOGOBUTTON,
-		STARTBUTTON,
-		YBUTTON,
-		BBUTTON,
-		ABUTTON,
-		XBUTTON,
-		LEFTBUTTON,
-		RIGHTBUTTON,
-		LEFTTRIGGER,
-		RIGHTTRIGGER
-	};
-	enum ButtonState {
-		PRESSED,
-		ACTIVE,
-		RELESASED,
-		NORMAL
-	};
+enum JoystickAxis {
+	LEFTSTICKX,
+	LEFTSTICKY,
+	RIGHTSTICKX,
+	RIGHTSTICKY,
+	RIGHTTRIGGERAXIS,
+	LEFTTRIGGERAXIS
+};
+enum JoystickButton {
+	DPADN,
+	DPADNE,
+	DPADE,
+	DPADSE,
+	DPADS,
+	DPADSW,
+	DPADW,
+	DPADNW,
+	LEFTSTICK,
+	RIGHTSTICK,
+	BACKBUTTON,
+	LOGOBUTTON,
+	STARTBUTTON,
+	YBUTTON,
+	BBUTTON,
+	ABUTTON,
+	XBUTTON,
+	LEFTBUTTON,
+	RIGHTBUTTON,
+	LEFTTRIGGER,
+	RIGHTTRIGGER
+};
+enum ButtonState {
+	PRESSED,
+	ACTIVE,
+	RELEASED,
+	NORMAL
+};
 class COREJoystick : public CORETask {
 public:
-	COREJoystick(int joystickNumber)
-	{
-
-	}
+	COREJoystick(unsigned int joystickNumber);
 	void registerAxis(JoystickAxis axis);
 	void registerButton(JoystickButton button);
 	double getAxis(JoystickAxis axis);
 	bool getButton(JoystickButton button);
 	ButtonState getButtonState(JoystickButton button);
 	void preTeleopTask();
+private:
+	Joystick joystick;
+	map<JoystickButton, ButtonState> lastButtonCache, buttonCache;
+	map<JoystickAxis, double> axisCache;
 };
 
 }
