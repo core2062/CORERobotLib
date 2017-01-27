8a3a8e9061cc18e3bbbccc00f196f6361726d7fe
Andrew Kempen <akemperfi@gmail.com>
2017-01-26 18:14:31 -0600

    COREDashboard Creation

---
+++ src/COREDashboard.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "WPILib.h"
+
+using namespace std;
+
+namespace CORE {
+    class COREDashboard {
+    public:
+        COREDashboard(string name);
+        void putNumber(double value);
+        double getNumber();
+        void putString(string value);
+        string getString();
+        void putBoolean(bool value);
+        bool getBoolean();
+        string getName();
+        void setDefaultNumber(double value);
+        void setDefaultString(string value);
+        void setDefaultBoolean(bool value);
+    private:
+        string m_name;
+        double m_defaultNumber = 0;
+        string m_defaultString = "NULL";
+        bool m_defaultBoolean = false;
+    };
+}
