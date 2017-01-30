fc915ad70f70bde3b6b2c7ee18ca65253122d879
Andrew Kempen <akemperfi@gmail.com>
2017-01-29 21:53:46 -0600

    Removed old code

--- src/COREHardware.h
+++ src/COREHardware.h
@@ -8,22 +8,3 @@
 #include "COREHardware/COREEncoder.h"
 #include "COREHardware/COREJoystick.h"
 #include "COREHardware/CORESensor.h"
-#include "WPILib.h"
-/*
-using namespace CORE;
-using namespace std;
-
-namespace CORE {
-    class Robot {
-    public:
-        static void addMotor(COREMotor* motor);
-        static void addJoystick(COREJoystick* joystick);
-        static COREMotor* motor(int port);
-        static COREJoystick* joystick(int port);
-        static void disableAllMotors();
-        static void updateMotors();
-    private:
-        static map<int, COREMotor*> m_motorMap;
-        static map<int, COREJoystick*> m_joystickMap;
-    };
-}*/
