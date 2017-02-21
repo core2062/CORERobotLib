56aa9605f2cf82b31f11bd8bbc01f8e15501cad9
Andrew Kempen <akemperfi@gmail.com>
2017-02-21 17:37:57 -0600

    Fixed issues with motion profiles

--- src/COREController/COREMotionProfile.h
+++ src/COREController/COREMotionProfile.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include "../COREController.h"
+#include <memory>
+
+using namespace std;
 
 namespace CORE {
     class COREMotionProfile : COREMotionController {
     public:
         COREMotionProfile(ControllerInput* inputDevice, ControllerOutput* outputDevice, double maxVel, double maxAccel);
+        COREMotionProfile(shared_ptr<ControllerInput> inputDevice, shared_ptr<ControllerOutput> outputDevice, double maxVel, double maxAccel);
         COREMotionProfile(double maxVel, double maxAccel);
         void setMaxVel(double maxVelocity);
         void setMaxAccel(double maxAcceleration);
@@ -15,11 +19,12 @@ namespace CORE {
         double Get() override;
         void setActual(double actualPosition) override;
         double getActual() override;
-    private:
         void update(int profile) override;
+    private:
         double m_maxVel = -1;
         double m_maxAccel = -1;
         double m_maxJerk = -1;
+        double m_output = 0;
         double m_actualVelocity, m_maxOutputSpeed;
         double m_timeToAccel, m_ticksToAccel;
         CORETimer m_timer;
