f71479d3860814d892584f1bd84f40d956836640
Joshua Spleas <JoshuaSpleas@gmail.com>
2017-03-18 09:54:02 -0500

    Changed Pathloader to Work with new files

--- src/WaypointFollower/PathLoader.cpp
+++ src/WaypointFollower/PathLoader.cpp
@@ -7,50 +7,55 @@ Path * PathLoader::loadPath(std::string fileName, double speedScale, bool flipY,
     std::string fileStarter = "/home/lvuser/COREAutoPaths/";
     std::ifstream inFile(fileStarter + fileName);
     if (inFile.is_open()) {
-        while (std::getline(inFile, line)) {
-            while (line.find('\r') != std::string::npos) {
-                line.erase(line.find('\r'), 1);
+    		while (std::getline(inFile, line)) {
+    			while (line.find('\r') != std::string::npos) {
+    				line.erase(line.find('\r'), 1);
+    			}
+    			while (line.find('\n') != std::string::npos) {
+    				line.erase(line.find('\n'), 1);
+    			}
+    			DataPoint p;
+    			std::size_t comma = line.find(',');
+    			double * numToSet = &p.x;
+    			while (line.find(',') != std::string::npos) {
+    				std::size_t comma = line.find(',');
+    				std::string data = line.substr(0, comma);
+    				*numToSet = atof(data.c_str());
+    				line.erase(0, comma + 1);
+    				numToSet += 1;
+    			}
+    			if (line.find('"') != std::string::npos) {
+    				while (line.find('"') != std::string::npos) {
+    					line.erase(line.find('"'), 1);
+    				}
+    				p.event = line;
+    			}
+    			else {
+    				*numToSet = atof(line.c_str());
+    			}
+
+    			Waypoint wp;
+    			wp.position = Translation2d(p.x,p.y);
+    			wp.speed = 100;
+    			wp.event = p.event;
+    			points.push_back(wp);
+    		}
+            if(!points.empty()){
+            	std::cout << fileName << " has " << points.size() << " points" << std::endl;
+            	for(auto i : points){
+            		std::cout << i.position.getX() << " " << i.position.getY() << std::endl;
+            	}
+            	return new Path(points, flipY, flipX);
             }
-            while (line.find('\n') != std::string::npos) {
-                line.erase(line.find('\n'), 1);
+            else{
+            	std::cout << "File: " << fileName << " was empty!" << std::endl;
+            	return new Path({Waypoint({-1,-1}, -1)}, flipY, flipX);
             }
-            Waypoint p;
-            std::size_t comma = line.find(',');
-            std::string data = line.substr(0, comma);
-            p.position.setX(atof(data.c_str()));
-            line.erase(0, comma+1);
-            comma = line.find(',');
-            data = line.substr(0, comma);
-            p.position.setY(atof(data.c_str()));
-            line.erase(0, comma+1);
-            if (line.find(',') != std::string::npos) {
-                comma = line.find(',');
-                data = line.substr(0, comma);
-                p.speed = atof(data.c_str());
-                line.erase(0, comma+1);
-                while (line.find('"') != std::string::npos) {
-                    line.erase(line.find('"'), 1);
-                }
-                p.event = line;
-            }
-            else {
-                p.speed = atof(line.c_str());
-                p.speed *= speedScale;
-            }
-            points.push_back(p);
-        }
-        if(!points.empty()){
-        	std::cout << fileName << " has " << points.size() << " points" << std::endl;
-        	for(auto i : points){
-        		std::cout << i.position.getX() << " " << i.position.getY() << std::endl;
-        	}
-        	return new Path(points, flipY, flipX);
-        }
-        else{
-        	std::cout << "File: " << fileName << " was empty!" << std::endl;
-        	return new Path({Waypoint({-1,-1}, -1)}, flipY, flipX);
-        }
-    }
+    	}
+    	else {
+    		std::cout << "Could not find file: " << fileStarter + fileName << std::endl;
+    	}
+
     std::cout << "Failed to open: " << fileName << std::endl;
     return new Path({Waypoint({-1,-1}, -1)}, flipY, flipX);
 }
--- src/WaypointFollower/PathLoader.h
+++ src/WaypointFollower/PathLoader.h
@@ -7,6 +7,14 @@
 #include <vector>
 #include <stdlib.h>
 
+struct DataPoint {
+	double x = 0;
+	double y = 0;
+	double speed = 100;
+	double angle = -7;
+	std::string event = "";
+};
+
 class PathLoader{
 public:
 	static Path * loadPath(std::string fileName, double speedScale = 1.0, bool flipY = false, bool flipX = false);
